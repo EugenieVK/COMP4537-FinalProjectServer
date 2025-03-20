ecb34b0641c40ed60661b898e7231e55f3eb3e1b
Catherine Queenan <catherinerqueenan@gmail.com>
2025-03-19 20:09:34 -0700

    Dealt with all string constants and commented code

--- lang/en/en.js
+++ lang/en/en.js
@@ -2,5 +2,13 @@ exports.messages = {
     BadRequest: "Request could not be understood",
     DBError: "An error occurred editing the database",
     Logout: "Successful Logout!",
-    PageNotFound : "Page Not Found"
+    PageNotFound : "Page Not Found",
+    InvalidLogin: "Invalid email or password",
+    LoginSuccess: "Successful Login!",
+    EmailUsed: "Email already in use",
+    RegisterSuccess: "User Registered",
+    InvalidToken: "Access Denied: Invalid Token",
+    DatabaseInit: "Database Initialized!",
+    DatabaseError: "Error initializing database or server:",
+    ServerRunning: "Server is running at port {port}"
 }
